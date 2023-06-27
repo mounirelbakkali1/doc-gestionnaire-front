58b775e0ff149426a2a63f2656c100a92d66d989
Mounir El-Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-06-27 13:43:18 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateDocumentComponent } from './components/create-document/create-document.component';
+import { EditDocumentComponent } from './components/edit-document/edit-document.component';
+import { ListDocumentsComponent } from './components/list-documents/list-documents.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateDocumentComponent,
+    EditDocumentComponent,
+    ListDocumentsComponent,
+    SignUpComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
